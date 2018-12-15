1ff46fc30faee2f4548cbb48b3d40780c04a7523
Zackptg5 <zackptg5@comcast.net>
2018-12-15 01:50:50 -0500

    Use static flags, PIE is for dynamic

--- build-bash.sh
+++ build-bash.sh
@@ -114,8 +114,7 @@ export LD=$target_host-ld
 export STRIP=$target_host-strip
 
 # Tell configure what flags Android requires.
-export CFLAGS="-fPIE -fPIC"
-export LDFLAGS="-pie -s"
+export LDFLAGS="-static -s"
 
 # Apply bash patches - Android patches originally by Alexander Gromnitsky, ATechnoHazard, koro666, and Termux @Github
 apply_patches
